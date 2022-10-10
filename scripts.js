39eb25b80a5f5260d79af31d6b0ebced5eea2249
sarahhoppe19 <sarah.hoppe@wsu.edu>
2022-10-10 02:30:41 +0000

    updated all files to complete TP6

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
-//JavaScript for TP5
+//JavaScript for TP6
+
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML += this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
 
 window.onload = function() {
+  loadFileInto("ingredients.html", "#ingredients ul");
+  loadFileInto("equipment.html", "#equipment ul");
+  loadFileInto("directions.html", "#directions ol");
   
   document.querySelector("#recipeBanner h1").classList.add("tp");
   
@@ -21,4 +48,5 @@ window.onload = function() {
   }
   
   document.querySelector("#recipeColumns #ingredients ul").innerHTML += "<li>Feel free to add extra ingredients to your liking!</li>";
+
 } //end of window.onload
